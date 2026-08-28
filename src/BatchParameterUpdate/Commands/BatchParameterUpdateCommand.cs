86487ac062d46678a2aec4071857b8a3adf783c4
Juan Sebastian Fuerte <jsfuertem@unal.edu.co>
2026-08-28 12:51:50 -0600

    feat: add minimal external command and addin manifest

---
+++ src/BatchParameterUpdate/Commands/BatchParameterUpdateCommand.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace BatchParameterUpdate.Commands;
+
+[Transaction(TransactionMode.Manual)]
+public class BatchParameterUpdateCommand : IExternalCommand
+{
+    public Result Execute(
+        ExternalCommandData commandData,
+        ref string message,
+        ElementSet elements)
+    {
+        TaskDialog.Show("Batch Parameter Update", "The add-in loaded correctly.");
+        return Result.Succeeded;
+    }
+}
